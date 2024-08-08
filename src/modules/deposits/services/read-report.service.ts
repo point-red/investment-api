cd2c8abc8ec3ca1f20109df082daa75510f577e0
habibmuhammadthariq <thariqhabibmuhammad@gmail.com>
2024-08-09 00:52:45 +0700

    update placement type filter rules on investment report

--- src/modules/deposits/services/read-report.service.ts
+++ src/modules/deposits/services/read-report.service.ts
@@ -63,14 +63,14 @@ export class ReadReportService {
       try {
         if (query.filter["placementType"] == "all") {
         } else if (query.filter["placementType"] == "renewal") {
-          query.filter["deposit_id"] = { $exists: true };
+          query.filter = {
+            ...query.filter,
+            $and: [{ withdrawal: { $exists: false } }, { renewal_id: { $exists: false } }],
+          };
         } else if (query.filter["placementType"] == "withdrawn") {
           query.filter["withdrawal"] = { $exists: true };
         } else if (query.filter["placementType"] == "placement") {
-          query.filter = {
-            ...query.filter,
-            $and: [{ withdrawal: { $exists: false } }, { deposit_id: { $exists: false } }],
-          };
+          query.filter["deposit_id"] = { $exists: false };
         }
         delete query.filter["placementType"];
       } catch (e) {}
@@ -122,8 +122,6 @@ export class ReadReportService {
       //     $replaceRoot: { newRoot: "$deposits" },
       //   },
     ];
-    console.log(pipeline)
-    console.log(query)
 
     const depositRepository = new DepositRepository(this.db);
     const result = (await depositRepository.aggregate(pipeline, query)) as ReadManyResultInterface;
